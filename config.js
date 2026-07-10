5f0e65b907b981af41f091f85ecbc08d93816a00
mariasys-ty <markmellon34@gmail.com>
2026-07-10 17:44:18 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+var ENV_CONFIG = {
+ FIREBASE_API_KEY: "AIzaSyCvrWHOHXmVmHkl451aQA6XFCWy7xA9jFw",
+ FIREBASE_AUTH_DOMAIN: "xstream-9f21e.firebaseapp.com",
+ FIREBASE_PROJECT_ID: "xstream-9f21e",
+ FIREBASE_STORAGE_BUCKET: "xstream-9f21e.firebasestorage.app",
+ FIREBASE_MESSAGING_SENDER_ID: "179015046758",
+ FIREBASE_APP_ID: "1:179015046758:web:e7da8143826b59b49e7fa7",
+ FIREBASE_MEASUREMENT_ID: "G-RN9E44VEFY",
+ ADMIN_UID: "XtfmzZ0n3xXEcAykk1GcMHc7rsw1",
+ OMDB_API_KEY: "9507c102",
+ OMDB_API_URL: "https://www.omdbapi.com/",
+ EMAILJS_PUBLIC_KEY: "wycdpK4PfADIaUPHn",
+ EMAILJS_SERVICE_ID: "service_fastmail",
+ EMAILJS_TEMPLATE_ID: "template_6gmm9xh",
+ // Add these inside var ENV_CONFIG = { ... }
+ EMAILJS_FORGOT_SERVICE_ID: "service_fhb6qha", // You can use the same service as OTP
+ EMAILJS_FORGOT_TEMPLATE_ID: "template_4c189h9", // The ID of your new Forgot Password template
+ INFOBIP_BASE_URL: 'y4x2lg.api.infobip.com',
+ INFOBIP_API_KEY: '60eba8332e0e3f0108cb8b201f32cd12-3f63d9de-a8bf-4b99-a3ca-641e0f3c5c49',
+ INFOBIP_SENDER: '+447860088970' // Your approved Infobip sender number
+};
